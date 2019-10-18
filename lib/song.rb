946983ce97f491ba79db9bad25f2bc01df0966fc
Becky Zorn <bczorn@gmail.com>
2019-10-18 18:46:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -58,7 +58,7 @@ class Song
     b = b.split(".")
     name = b[0].to_s
     self.create_by_name(name)
-    self.create_by_artist_name(artist_name)
+    name
   end
   
   def self.create_from_filename
