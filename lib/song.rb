c7c345e70e65390a588de5ffe203844e7484c8d6
Becky Zorn <bczorn@gmail.com>
2019-10-18 18:56:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -62,8 +62,8 @@ class Song
 
   end
   
-  def self.create_from_filename
-    self.
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename)
   end
   
   def self.destroy_all
